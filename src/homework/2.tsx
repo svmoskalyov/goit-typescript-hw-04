7a7bee5463fdb4ccd47d997be29f56ba53125cee
Serhii Moskalov <sv.moskalyov@gmail.com>
2023-08-26 12:50:18 +0300

    Update 2.tsx

--- src/homework/2.tsx
+++ src/homework/2.tsx
@@ -1,4 +1,13 @@
-import React, {useReducer} from "react";
+import React, { useReducer } from "react";
+
+type State = {
+  isRequestInProgress: boolean;
+  requestStep: 'start' | 'pending'| 'finished' | 'idle';
+}
+
+type Action = {
+  type: 'START_REQUEST' | 'PENDING_REQUEST' | 'FINISH_REQUEST' | 'RESET_REQUEST';
+}
 
 const initialState: State = {
   isRequestInProgress: false,
